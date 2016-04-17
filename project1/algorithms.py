ea9136007dfeeb88a2635d6fb8a6bc01e60520e9
Jordan Crane <cranejo@oregonstate.edu>
2016-04-17 16:09:05 -0700

    Fixed bugs in algorithm 3. TODO: retrieve correct indices from algorithm 3.

--- project1/algorithms.py
+++ project1/algorithms.py
@@ -1,3 +1,5 @@
+from math import floor
+
 #Algorithm 1
 def enum_msa(array=[]):
     maxSum = 0
@@ -33,28 +35,30 @@ def betterEnum_msa(array=[]):
     return startIdx, stopIdx, maxSum
 
 #Algorithm 3
-def recursive_msa(left_bound, right_bound, array=[]):
-    left_max, mid_max, right_max, sub_max, total_sum = 0
-    max_left_side, max_right_side = -1000000, -1000000
+def recursive_msa(left_bound=0, right_bound=0, array=[]):
+    left_max, mid_max, right_max, sub_max, total_sum = [0,0,0,0,0]
+    max_left_side, max_right_side = [-1000000, -1000000]
     
     #Check if the is only one variable left
     if (left_bound == right_bound):
         return array[left_bound]
     
     #Find the maximum subarrays on the left and right
-    left_max = recursive_msa(left_bound,(left_bound+right_bound)/2, array)
-    right_max = recursive_msa((left_bound+right_bound)/2+1,right_bound, array)
+    print "Calling recursive_msa from ", left_bound+int(floor((right_bound-left_bound)/2))+1, " to ", right_bound, " for right max"
+    right_max = recursive_msa(left_bound+int(floor((right_bound-left_bound)/2))+1, right_bound, array)
+    print "Calling recursive_msa from ", left_bound, " to ", left_bound+int(floor((right_bound-left_bound)/2)), " for left max"
+    left_max = recursive_msa(left_bound, left_bound+int(floor((right_bound-left_bound)/2)), array)
     
     #Maximum for a middle sub array array
     #Find max on left side
-    for i in range((left_bound+right_bound)/2, left_bound-1, -1):
+    for i in range(left_bound+int(floor((right_bound-left_bound)/2)), left_bound-1, -1):
         total_sum += array[i]
         #Update the left side maximum
         if (total_sum > max_left_side):
             max_left_side = total_sum
     total_sum = 0
     #Find max on right side
-    for i in range((left_bound+right_bound)/2+1,right_bound+1):
+    for i in range(left_bound+int(floor((right_bound-left_bound)/2))+1,right_bound+1):
         total_sum += array[i]
         #Update the right side maximum
         if (total_sum > max_right_side):
@@ -68,8 +72,10 @@ def recursive_msa(left_bound, right_bound, array=[]):
     return sub_max
 
 def recursive_msa_call(array=[]):
-    max = recursive_msa(0,len(array), array)
-    return max
+    start_idx = 0
+    end_idx = 0
+    max = recursive_msa(0,len(array)-1, array)
+    return start_idx, end_idx, max
 
 
 #Algorithm 4

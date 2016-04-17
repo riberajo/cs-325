1e82f863f4c9bf9392244b1cdd710b12c59b6292
Jordan Crane <cranejo@oregonstate.edu>
2016-04-16 18:35:43 -0700

    Added algorithm 4 to algorithms.py

--- project1/algorithms.py
+++ project1/algorithms.py
@@ -1,3 +1,4 @@
+#Algorithm 1
 def enum_msa(array=[]):
     maxSum = 0
     startIdx = 0
@@ -16,7 +17,7 @@ def enum_msa(array=[]):
 
     return startIdx, stopIdx, maxSum
 
-
+#Algorithm 2
 def betterEnum_msa(array=[]):
     maxSum = 0
     startIdx = 0
@@ -30,3 +31,23 @@ def betterEnum_msa(array=[]):
                 startIdx = i
                 stopIdx = j
     return startIdx, stopIdx, maxSum
+
+#Algorithm 4
+def linearTime_msa(array=[]):
+    max_so_far = array[0]
+    max_ending_here = 0
+    start_idx, stop_idx, start_idx_so_far = 0    #Initial maximum subarray is array[0:0]
+
+    for i in (1, len(array)):
+        if(max_ending_here + array[i] > array[i]):  #should this be gt or gteq?
+            max_ending_here += array[i]
+        else:
+            max_ending_here = array[i]
+            start_idx_so_far = i
+
+        if(max_so_far < max_ending_here):   #should this be lt or lteq?
+            max_so_far = max_ending_here
+            start_idx = start_idx_so_far
+            stop_idx = i
+
+    return max_so_far

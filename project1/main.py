543728335e6e7d80934690138f1022301f1df4f4
Jordan Crane <cranejo@oregonstate.edu>
2016-04-16 18:43:28 -0700

    Added algorithm 4 calls to main.py

--- project1/main.py
+++ project1/main.py
@@ -6,16 +6,18 @@ def main():
     src = 'MSS_Results.txt'
     alg1name = 'Enumeration '
     alg2name = 'Better Enumeration '
+    alg4name = 'Linear Time'
 
     #src, algo, algo name
     getResults(src, enum_msa, alg1name)
     getResults(src, betterEnum_msa, alg2name)
+    getResults(src, linearTime_msa, alg4name)
 
     #uses randomly generated array and compares them to each other
-    randomTest(enum_msa, betterEnum_msa)
+    randomTest(enum_msa, betterEnum_msa, linearTime_msa)
 
     #gets running times for various iteration numbers
-    plotAlltheTimes(enum_msa, betterEnum_msa, alg1name, alg2name)
+    plotAlltheTimes(enum_msa, betterEnum_msa, linearTime_msa, alg1name, alg2name, alg4name)
     
 if __name__ == "__main__":
     main()

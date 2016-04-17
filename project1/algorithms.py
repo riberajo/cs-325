4e30d911adf20b13f684abd804edd679a237c692
Jordan Crane <cranejo@oregonstate.edu>
2016-04-17 16:26:08 -0700

    Added index return to algorithm 3

--- project1/algorithms.py
+++ project1/algorithms.py
@@ -41,13 +41,13 @@ def recursive_msa(left_bound=0, right_bound=0, array=[]):
     
     #Check if the is only one variable left
     if (left_bound == right_bound):
-        return array[left_bound]
+        return right_bound, left_bound, array[left_bound]
     
     #Find the maximum subarrays on the left and right
     print "Calling recursive_msa from ", left_bound+int(floor((right_bound-left_bound)/2))+1, " to ", right_bound, " for right max"
-    right_max = recursive_msa(left_bound+int(floor((right_bound-left_bound)/2))+1, right_bound, array)
+    start_idx_right, stop_idx_right, right_max = recursive_msa(left_bound+int(floor((right_bound-left_bound)/2))+1, right_bound, array)
     print "Calling recursive_msa from ", left_bound, " to ", left_bound+int(floor((right_bound-left_bound)/2)), " for left max"
-    left_max = recursive_msa(left_bound, left_bound+int(floor((right_bound-left_bound)/2)), array)
+    start_idx_left, stop_idx_left, left_max = recursive_msa(left_bound, left_bound+int(floor((right_bound-left_bound)/2)), array)
     
     #Maximum for a middle sub array array
     #Find max on left side
@@ -56,26 +56,35 @@ def recursive_msa(left_bound=0, right_bound=0, array=[]):
         #Update the left side maximum
         if (total_sum > max_left_side):
             max_left_side = total_sum
+            start_idx_mid = i
     total_sum = 0
+
     #Find max on right side
     for i in range(left_bound+int(floor((right_bound-left_bound)/2))+1,right_bound+1):
         total_sum += array[i]
         #Update the right side maximum
         if (total_sum > max_right_side):
             max_right_side = total_sum
+            stop_idx_mid = i
     
     mid_max = max_left_side + max_right_side
 
     #Finds the biggest of the three and returns it
-    sub_max = max(max(left_max,right_max),mid_max)
-    
-    return sub_max
+    if(left_max > right_max):
+        sub_max = left_max
+        start_idx, stop_idx = [start_idx_left, stop_idx_left]
+    else:
+        sub_max = right_max
+        start_idx, stop_idx = [start_idx_right, stop_idx_right]
+    if(mid_max > sub_max):
+        sub_max = mid_max
+        start_idx, stop_idx = [start_idx_mid, stop_idx_mid]
+
+    return start_idx, stop_idx, sub_max
 
 def recursive_msa_call(array=[]):
-    start_idx = 0
-    end_idx = 0
-    max = recursive_msa(0,len(array)-1, array)
-    return start_idx, end_idx, max
+    start_idx, stop_idx, max = recursive_msa(0,len(array)-1, array)
+    return start_idx, stop_idx, max
 
 
 #Algorithm 4
